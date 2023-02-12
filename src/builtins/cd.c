283391a126a56632e84f3d34f8a2c018265c2207
kx <kx@imkx.dev>
2023-02-13 00:50:56 +0100

    feat: made cd work with environment

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,33 +6,33 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 00:02:29 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/13 00:10:09 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/02/13 00:49:18 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <stdlib.h>
 #include "builtins.h"
 #include "libft.h"
+#include "environment.h"
 
-// TODO: support cd - and cd ~ using get_env and set_env
 int	cd_builtin(t_shell *shell, t_cmd *cmd)
 {
 	char	*path;
-	//char	*oldpwd;
-	//char	*pwd;
+	char	*oldpwd;
+	char	*pwd;
 
-	(void)shell;
 	if (cmd->argc > 2)
 	{
 		ft_putstr_fd("cd: too many arguments\n", 2);
 		return (1);
 	}
-	//if (cmd->argc == 1)
-	//	path = get_env(shell->env, "HOME");
-	//else
-	path = cmd->args[1];
+	if (cmd->argc == 1)
+		path = get_env(shell->env, "HOME");
+	else
+		path = cmd->args[1];
 	if (!path)
 	{
 		ft_putstr_fd("cd: HOME not set\n", 2);
@@ -47,11 +47,11 @@ int	cd_builtin(t_shell *shell, t_cmd *cmd)
 		ft_putstr_fd("\n", 2);
 		return (1);
 	}
-	//oldpwd = get_env(shell->env, "PWD");
-	//pwd = getcwd(NULL, 0);
-	//set_env(shell->env, "OLDPWD", oldpwd);
-	//set_env(shell->env, "PWD", pwd);
-	//free(oldpwd);
-	//free(pwd);
+	oldpwd = get_env(shell->env, "PWD");
+	pwd = getcwd(0, 0);
+	set_env(shell, "OLDPWD", oldpwd);
+	set_env(shell, "PWD", pwd);
+	free(oldpwd);
+	free(pwd);
 	return (0);
 }
