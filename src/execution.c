4cc715f32be62d711d27d02576d56445b120c08b
kx <kx@imkx.dev>
2023-02-12 23:26:59 +0100

    fix: better check for exit keyword

--- src/execution.c
+++ src/execution.c
@@ -6,7 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 23:08:07 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/12 23:22:41 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/02/12 23:26:36 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	check_builtin(t_shell *shell, char *line, int *status)
 {
-	if (ft_strncmp(line, "exit", 4) == 0)
+	if (ft_memcmp(line, "exit", 5) == 0 || ft_memcmp(line, "exit ", 5) == 0)
 	{
 		shell->exit = 1;
 		*status = 0;
