d62d0d9d521955996dd08a116abf2b21c78bbb6a
kx <kx@imkx.dev>
2023-02-13 04:01:12 +0100

    feat: trim leading spaces or tabs from command

--- src/parsing/parse_command.c
+++ src/parsing/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 23:27:19 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/12 23:51:37 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/02/13 04:00:15 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@
 int	parse_command(char *line, t_cmd *cmd)
 {
 	int	i;
+	int	start;
 
 	i = 0;
+	while (line[i] && (line[i] == ' ' || line[i] == '\t'))
+		i++;
+	start = i;
 	while (line[i] && line[i] != ' ')
 		i++;
-	cmd->name = ft_substr(line, 0, i);
+	cmd->name = ft_substr(line, start, i);
 	cmd->args = ft_split(line, ' ');
 	cmd->argc = ft_arraylen(cmd->args);
 	return (0);
