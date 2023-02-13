0146927e1587c35b9ead908892d27123329b285d
kx <kx@imkx.dev>
2023-02-13 01:11:26 +0100

    feat: added readonly get_env

--- includes/environment.h
+++ includes/environment.h
@@ -6,7 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 23:03:49 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/13 00:37:45 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/02/13 01:03:03 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 #include "types.h"
 
-void	load_env(t_shell *shell, char **envp);
-char	*get_env(t_env *environment, char *key);
-void	set_env(t_shell *shell, char *key, const char *value);
-void	clear_env(t_env **environment);
+void		load_env(t_shell *shell, char **envp);
+char		*get_env(t_env *environment, char *key);
+const char	*get_env_nc(t_env *environment, char *key);
+void		set_env(t_shell *shell, char *key, const char *value);
+void		clear_env(t_env **environment);
 
 #endif //ENVIRONMENT_H
--- src/env/get_env.c
+++ src/env/get_env.c
@@ -6,19 +6,7 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 00:14:52 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/13 00:49:47 by kschmidt         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_env.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/13 00:14:52 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/13 00:14:52 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/02/13 01:02:52 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +23,20 @@ char	*get_env(t_env *environment, char *key)
 	{
 		if (ft_strncmp(env->name, key, ft_strlen(key)) == 0)
 			return (ft_strdup(env->value));
+		env = env->next;
+	}
+	return (0);
+}
+
+const char	*get_env_nc(t_env *environment, char *key)
+{
+	t_env	*env;
+
+	env = environment;
+	while (env)
+	{
+		if (ft_strncmp(env->name, key, ft_strlen(key)) == 0)
+			return (env->value);
 		env = env->next;
 	}
 	return (0);
