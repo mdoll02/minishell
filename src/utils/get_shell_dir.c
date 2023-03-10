40f39d6d49371d6bee76512d6f68725657d666f9
kx <kx@imkx.dev>
2023-03-10 17:25:22 +0100

    feat: made get_shell_dir use getcwd instead of secret env

--- src/utils/get_shell_dir.c
+++ src/utils/get_shell_dir.c
@@ -6,24 +6,33 @@
 /*   By: kschmidt <kevin@imkx.dev>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 01:05:35 by kschmidt          #+#    #+#             */
-/*   Updated: 2023/02/15 17:53:36 by kschmidt         ###   ########.fr       */
+/*   Updated: 2023/03/10 17:21:15 by kschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <malloc.h>
 #include "utils.h"
 #include "environment.h"
 #include "libft.h"
 
+#define MAX_PATH_LEN 1024
+
 char	*get_shell_dir(t_shell *shell)
 {
-	const char	*shell_dir;
+	char		*shell_dir;
 	const char	*home_dir;
 	size_t		home_dir_len;
 
-	shell_dir = get_env_nc(shell->secret_env, "PWD");
+	shell_dir = malloc(MAX_PATH_LEN * sizeof(char));
+	shell_dir = getcwd((char *)shell_dir, MAX_PATH_LEN);
 	home_dir = get_env_nc(shell->secret_env, "HOME");
 	home_dir_len = ft_strlen(home_dir);
 	if (ft_strncmp(shell_dir, home_dir, home_dir_len) == 0)
-		return (ft_strjoin("~", shell_dir + ft_strlen(home_dir)));
-	return (get_env(shell->secret_env, "PWD"));
+	{
+		shell_dir[0] = '~';
+		ft_memmove(shell_dir + 1, shell_dir + home_dir_len,
+			ft_strlen(shell_dir + home_dir_len) + 1);
+	}
+	return (shell_dir);
 }
