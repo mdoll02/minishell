6696e27a3b5ed721e9f5362ebe155912878e4b98
mdoll <mdoll@student.42wolfsburg.de>
2023-03-10 12:30:51 +0100

    refactor: changed pipes to (maybe) work with codebase

--- src/pipes/create_pipes.c
+++ src/pipes/create_pipes.c
@@ -12,40 +12,13 @@
 
 // I need:
 // argc, argv, envp
-// TODO get them pipes working
+// TODO need to test code
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "types.h"
-
-// line 37 / 38 for syntax error?
-static int	nb_of_pipes(char **args)
-{
-	int		nb_of_pipes;
-	int		index_args;
-	int		index_chars;
-
-	nb_of_pipes = 0;
-	index_args = 0;
-	while (args[index_args])
-	{
-		index_chars = 0;
-		while (args[index_args][index_chars])
-		{
-			if (args[index_args][index_chars] == '|')
-				nb_of_pipes++;
-			index_chars++;
-			if (nb_of_pipes != 1)
-				return (-1);
-		}
-		index_args++;
-	}
-	return (nb_of_pipes);
-}
-
-int	pipes(t_shell *shell, t_cmd *cmd)
-{
-	t_pipex	pipex;
-	int		cmd_count;
+#include "execution.h"
 
 void	call_child_process(t_cmd *cmd, int counter, t_shell *shell, int *status)
 {
@@ -75,29 +48,21 @@ void	call_child_process(t_cmd *cmd, int counter, t_shell *shell, int *status)
 	}
 }
 
-void	open_files(t_pipex *pipex, char **argv, int argc)
+int	pipes(t_shell *shell, t_cmd *cmd)
 {
-	pipex->fd_outfile = open(argv[argc - 1], O_WRONLY | \
-		O_TRUNC | O_CREAT, 0644);
-	if (pipex->fd_outfile == -1)
-		error_msg(argv[argc - 1], -1);
-	pipex->fd_infile = open(argv[1], O_RDONLY);
-	if (pipex->fd_infile == -1)
-		error_msg(pipex->argv[1], pipex->fd_outfile);
-}
+	int	counter;
+	int	status;
 
-
-void	error(char *str)
-{
-	perror(str);
-	exit(1);
-}
-
-void	error_msg(char *str, int fd)
-{
-	if (fd != -1)
-		write(fd, "       0\n", 9);
-	write(STDERR_FILENO, "pipex: ", 7);
-	perror(str);
-	exit(0);
+	counter = 0;
+	dup2(CT_RED_IN, STDIN_FILENO);
+	while (counter < cmd->nb_pipes)
+	{
+		call_child_process(cmd, counter, shell, &status);
+		if (status == -1)
+			return (status);
+		counter++;
+	}
+	dup2(CT_RED_OUT, STDOUT_FILENO);
+	execute(shell, cmd->args[counter], &status);
+	return (status);
 }
